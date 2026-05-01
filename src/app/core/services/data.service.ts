a21b41d9a2ecf01dd1f79ea9a49caec10fdf6133
DuyHieu2004 <doanhieu.11052004@gmail.com>
2026-05-01 15:43:00 +0700

    refactor: update page for product is hidden Add Into Cart if it already in Cart

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -48,6 +48,11 @@ export class DataService {
   constructor() {
     this.loadCategories();
     this.loadCourses();
+
+    if (this.auth.isLoggedIn()) {
+      this.loadCart();
+      this.loadMyCourses();
+    }
   }
 
   // ========================
